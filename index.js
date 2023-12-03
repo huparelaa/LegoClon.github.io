ec16d0cdbbfd9752d47b72ed07969783c6cb3f84
Hobar <huparelaa@eafit.edu.co>
2023-12-03 11:38:31 -0500

    feat: full page

---
+++ index.js
@@ -0,0 +1,24 @@
+const modal = document.querySelector('.modal')
+const button1 = document.querySelector('#button1')
+const button2= document.querySelector('#button2')
+const button3= document.querySelector('#button3')
+const closeButton = document.querySelector('.modal__content__close')
+
+
+button1.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+})
+button2.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+})
+button3.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+})
+
+closeButton.addEventListener('click', () => {
+    modal.classList.remove('visible');
+    modal.classList.add('hidden');
+})
